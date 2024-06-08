ff12d96be2015817c7e00533ed99aac2666c0f3d
s0zzang <dlth8888@gmail.com>
2024-06-08 16:43:27 +0900

    ✨ [FE] feat: 카카오맵, 수파베이스 연동 및 핀 클릭 이벤트 추가

--- components/kakao-map.tsx
+++ components/kakao-map.tsx
@@ -1,13 +1,42 @@
 import Script from "next/script";
 import { Map } from "react-kakao-maps-sdk";
+import EventMarkerContainer from "./marker-click";
+
+type Pin = {
+  emotion: number;
+  content: string;
+  gps: {
+    lat: number;
+    lng: number;
+  };
+  date: string;
+  location: string;
+  image: string;
+};
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_APP_JS_KEY}&autoload=false`;
 
-const KakaoMap = () => {
+const KakaoMap = ({ data }: { data: Pin[] }) => {
+  const positions = data.map((marker) => ({
+    content: marker.content,
+    latlng: {
+      lat: marker.gps.lat,
+      lng: marker.gps.lng,
+    },
+  }));
+
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: "100vw", height: "100vh" }}></Map>
+      <Map center={{ lat: 37.51112, lng: 127.095973 }} style={{ width: "100vw", height: "50vh" }}>
+        {positions.map((marker) => (
+          <EventMarkerContainer
+            key={`EventMarkerContainer-${marker.latlng.lat}-${marker.latlng.lng}`}
+            position={marker.latlng}
+            content={marker.content}
+          />
+        ))}
+      </Map>
     </>
   );
 };
