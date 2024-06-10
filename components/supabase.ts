74aa29829d83ae6410bbf3e27dfb80fe6ec3d9ef
s0zzang <dlth8888@gmail.com>
2024-06-10 22:49:45 +0900

    🐛 [FE] fix: 브라우저용 환경변수로 변경

--- components/supabase.ts
+++ components/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://jyrfxniwlrpwpcdjivbe.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey as string);
