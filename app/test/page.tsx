cc39023982c7bddc3a7a160953a4a74b170849d3
JWJung-99 <39busy@naver.com>
2024-06-13 00:47:13 +0900

    💄 [FE] style: 폴더 구조 및 CSS 수정

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Button from "@/components/button";
-import Input from "@/components/input";
+import Button from "@/components/button/button";
+import Input from "@/components/input/input";
 
 const Test = () => {
   const handleClick = () => {
