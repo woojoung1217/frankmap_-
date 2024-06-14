0796bf212fb09d3fde094caf12196d15bcc84f6a
s0zzang <dlth8888@gmail.com>
2024-06-14 15:29:57 +0900

    💄 [FE] style: 바텀 시트 UI

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,31 @@
 import getMarkers from "@/components/home/get-markers";
 import KakaoMap from "@/components/home/kakao-map";
-import "./home.scss";
+import BottomSheet from "@/components/bottomSheet/bottomSheet";
+import "@/app/(home)/home.scss";
 
 const Home = async () => {
   const data = await getMarkers();
   const emotionLists = data.map((post) => (
     <li key={post.created_at}>
       <img src={`/emotion${post.emotion}.svg`} alt={`${post.emotion}`} />
-      <span className="location">{post.location}</span>
-      <span className="date">{post.date}</span>
-      <p className="content">{post.content}</p>
+      <div className="r-wr">
+        <div className="t-wr">
+          <span className="location">{post.location}</span>
+          <span className="date">{post.date}</span>
+        </div>
+        <div className="b-wr">
+          <p className="content">{post.content}</p>
+        </div>
+      </div>
     </li>
   ));
 
   return (
     <div className="map">
       <KakaoMap data={data} />
-      <div className="bottomSheet">
-        <ul>{emotionLists}</ul>
-      </div>
+      <BottomSheet>
+        <ul className="emotionLists">{emotionLists}</ul>
+      </BottomSheet>
     </div>
   );
 };
