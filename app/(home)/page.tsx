cba35e1cf138da0729c1055674580cdfa5773920
s0zzang <dlth8888@gmail.com>
2024-06-19 14:52:53 +0900

    ♻️ [FE] refactor: 메인 - 감정 목록 컴포넌트 분리

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,30 +1,16 @@
 import getMarkers from "@/components/home/get-markers";
 import KakaoMap from "@/components/home/kakao-map";
 import BottomSheet from "@/components/bottomSheet/bottomSheet";
+import EmotionList from "@/components/home/emotion-list";
 import "@/app/(home)/home.scss";
 
 const Home = async () => {
   const data = await getMarkers();
-  const emotionLists = data.map((post) => (
-    <li key={post.created_at}>
-      <img src={`/emotion${post.emotion}.svg`} alt={`${post.emotion}`} />
-      <div className="r-wr">
-        <div className="t-wr">
-          <span className="location">{post.location}</span>
-          <span className="date">{post.date}</span>
-        </div>
-        <div className="b-wr">
-          <p className="content">{post.content}</p>
-        </div>
-      </div>
-    </li>
-  ));
-
   return (
     <div className="map">
       <KakaoMap data={data} />
       <BottomSheet>
-        <ul className="emotionLists">{emotionLists}</ul>
+        <EmotionList />
       </BottomSheet>
     </div>
   );
