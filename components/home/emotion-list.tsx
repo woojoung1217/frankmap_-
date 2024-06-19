40c2cd4a5a699da0f52ee11c255817c63a30e5f8
s0zzang <dlth8888@gmail.com>
2024-06-19 14:51:08 +0900

    ✨ [FE] feat: 카카오맵 - 현재 보고있는 영역의 데이터만 호출

---
+++ components/home/emotion-list.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRecoilValue } from "recoil";
+import { FilteredData } from "./filtered-data";
+
+const EmotionList = () => {
+  const handleClick = (post: RecordType) => {
+    console.log(post);
+  };
+  const data = useRecoilValue(FilteredData);
+
+  const emotionLists = data.map((post) => (
+    <li key={post.created_at} onClick={() => handleClick(post)}>
+      <img src={`/emotion${post.emotion}.svg`} alt={`${post.emotion}`} />
+      <div className="r-wr">
+        <div className="t-wr">
+          <span className="location">{post.location}</span>
+          <span className="date">{post.date}</span>
+        </div>
+        <div className="b-wr">
+          <p className="content">{post.content}</p>
+        </div>
+      </div>
+    </li>
+  ));
+  return <ul className="emotionLists">{data.length ? emotionLists : <p>감정을 기록해보세요. :)</p>}</ul>;
+};
+
+export default EmotionList;
