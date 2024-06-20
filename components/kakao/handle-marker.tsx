709adfa9bdceddb8c5613b5addf35aca073aa00e
s0zzang <dlth8888@gmail.com>
2024-06-20 21:36:46 +0900

    ✨ [FE] feat: 지도 - 검색용 마커 구분

--- components/kakao/handle-marker.tsx
+++ components/kakao/handle-marker.tsx
@@ -1,26 +1,36 @@
 "use client";
 import { MapMarker, useMap } from "react-kakao-maps-sdk";
 
-const EventMarkerContainer = ({ position, emotion }: { position: { lat: number; lng: number }; emotion: number }) => {
+const EventMarkerContainer = ({
+  type,
+  position,
+  emotion,
+}: {
+  type: string;
+  position: { lat: number; lng: number };
+  emotion?: number;
+}) => {
   const map = useMap();
-  const onClick = (marker) => {
+  const handleClick = (marker, position: { lat: string; lng: string }) => {
     map.panTo(marker.getPosition());
+    console.log(position);
   };
+
   return (
     <MapMarker
       position={position} // 마커를 표시할 위치
       // @ts-ignore
-      onClick={(marker) => onClick(marker, position)}
+      onClick={(marker) => handleClick(marker, position)}
       image={{
-        src: `/emotion${emotion}.svg`,
+        src: type === "default" ? `/emotion${emotion}.svg` : `/icon-marker.svg`,
         size: {
-          width: 50,
-          height: 50,
+          width: type === "default" ? 50 : 28,
+          height: type === "default" ? 50 : 40,
         },
         options: {
           offset: {
-            x: 27,
-            y: 69,
+            x: type === "default" ? 25 : 14,
+            y: type === "default" ? 25 : 20,
           },
         },
       }}
