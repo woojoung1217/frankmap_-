ca43ec2a7f87eddd32fe2a26f4ee863745824974
930_10 <930_10@naver.com>
2024-06-22 01:11:53 +0900

    ✨ [FE] feat: 모달 기능 구현

--- atoms/atoms.ts
+++ atoms/atoms.ts
@@ -1,8 +1,28 @@
 // src/recoil/atoms.mjs
-import { atom } from "recoil";
+import { atom, atomFamily } from "recoil";
 import getMarkers from "../components/kakao/get-markers";
+
+interface ModalType {
+  isOpen: Boolean;
+  title?: string;
+  content?: JSX.Element | string;
+  button?: string;
+  data?: string;
+  image?: string;
+  callBack?: () => any;
+}
 
 export const FilteredData = atom({
   key: "filteredData",
   default: getMarkers(),
+});
+
+export const modalState = atom<ModalType>({
+  key: "modalState",
+  default: {
+    isOpen: false,
+    title: "",
+    content: "",
+    button: "",
+  }
 });
