4fc749a302d68119478421aca1fca0a9dfa36924
woojoung <ywj981217@naver.com>
2024-06-25 16:17:51 +0900

    🐛 [FE] fix: 로그인 문법 오류 수정

--- atoms/userstate.ts
+++ atoms/userstate.ts
@@ -3,11 +3,11 @@
 import { atom } from "recoil";
 
 interface User {
-  email: string;
-  // 여기에 필요한 다른 사용자 정보도 추가할 수 있습니다.
+  id: string;
+  // 필요한 다른 필드들 추가
 }
 
-export const userState = atom<User | null>({
+export const userState = atom<User | string>({
   key: "userState",
-  default: null,
+  default: "",
 });
