c284d036de15519d2597d8ea229baa52182ecb7b
930_10 <930_10@naver.com>
2024-06-25 14:49:44 +0900

    🚧 [FE] cont: 모달 내 버튼 없을 시 처리, 코드 간소화, 데이터 렌더링 처리

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -1,25 +1,30 @@
 import { useModal } from "@/hooks/useModal";
 import "@/components/modal/modal.scss"
 import Button from "../button/button";
+import parse from 'html-react-parser';
 
 const Modal = () => {
   const { modalDataState, closeModal } = useModal();
-
+  const { title, content, button, callBack, isOpen } = modalDataState;
 
   return (
     <>
-      { modalDataState.isOpen && (
+      { isOpen && (
         <div className="modal-container">
           <button className="modal-close-button" onClick={closeModal}>닫기</button>
           <div className="modal-title">
-            <h1>{modalDataState.title}</h1>
+            <h1>{title}</h1>
           </div>
           <div className="modal-content">
-            <div>{modalDataState.content}</div>
+            <div>{typeof content === 'string' ? parse(content) : content}</div>
           </div>
-          <div className="modal-footer">
-            <Button handleClick={modalDataState.callBack} size="normal" color="secondary">{modalDataState.button}</Button>
-          </div>
+          {
+            button && (
+              <div className="modal-footer">
+                <Button handleClick={callBack} size="normal" color="secondary">{button}</Button>
+              </div>
+            )
+          }
         </div>
       )}
     </>
