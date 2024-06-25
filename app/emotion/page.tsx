a54de8eefacafa625e02d1804743e25ad1b0eac8
930_10 <930_10@naver.com>
2024-06-25 20:33:24 +0900

    🚧 [FE] cont: [진행 중] 감정 목록

--- app/emotion/page.tsx
+++ app/emotion/page.tsx
@@ -1,5 +1,52 @@
+"use client"
+
+
+
+import { supabase } from '@/libs/supabase';
+import { useEffect, useState } from 'react';
+
 const Emotion = () => {
-  return <div>Emotion</div>;
+  
+  const [records, setRecords] = useState<RecordType[]>();
+  
+  useEffect(() => {
+    const fetchRecords = async () => {
+      try {
+        const { data: record, error } = await supabase
+        .from('record')
+        .select('*')
+
+        setRecords(record as RecordType[]);
+
+      } catch (error) {
+        console.log('Error: fetching data');
+      }
+    }
+
+    fetchRecords();
+  }, []);
+
+  console.log(records);
+  
+
+
+  return (
+    <>
+      {records === undefined ? (
+        <div>로딩 중...</div>
+      ) : records.length > 0 ? (
+        <div>
+          {records.map((record) => (
+            <div key={record.record_id}>
+              <div>{record.content}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>기록이 없습니다.</div>
+      )}
+    </>
+  );
 };
 
 export default Emotion;
