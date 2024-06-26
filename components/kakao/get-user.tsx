47a6cba45c3854c13386a37a926b44e43a9068ac
s0zzang <dlth8888@gmail.com>
2024-06-26 20:07:25 +0900

    🚧 [FE] cont: 로그인 된 회원 데이터만 노출, 사용자 위치 정보 수정

--- components/kakao/get-user.tsx
+++ components/kakao/get-user.tsx
@@ -1,21 +1,23 @@
 "use client";
+import { dataState } from "@/atoms/atoms";
 import { userState } from "@/atoms/userstate";
 import fetchData from "@/libs/fetch-record";
 import { useEffect } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
-const GetUser = ({ onUserDataFetched }: { onUserDataFetched: React.Dispatch<React.SetStateAction<RecordType[]>> }) => {
+const GetUser = () => {
   const loggedInUserId = useRecoilValue(userState);
+  const setData = useSetRecoilState(dataState);
 
   useEffect(() => {
     const getData = async () => {
       if (loggedInUserId) {
         try {
           const resultData = await fetchData(loggedInUserId);
-          onUserDataFetched(resultData); // 부모 컴포넌트에 데이터 전달
+          setData(resultData); // 부모 컴포넌트에 데이터 전달
         } catch (error) {
           console.error("데이터 가져오기 오류:", error);
-          onUserDataFetched([]); // 오류 발생 시 빈 배열 전달
+          setData([]); // 오류 발생 시 빈 배열 전달
         }
       } else {
         // 사용자가 로그인되지 않은 경우 처리
