a7670945f1e495f89bbf9eddb7d01e6ea65c24c6
Dain <danggin.min@gmail.com>
2024-06-29 18:37:46 +0900

    💄 [FE] style: loading 색상 수정

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,10 @@
-// import "@/styles/loading.css";
+import "@/styles/loading.css";
 
 const Loading = () => {
   return (
     <>
       <div className="loader-wrapper">
-        <div className="loader">로딩중</div>
+        <div className="loader"></div>
       </div>
     </>
   );
