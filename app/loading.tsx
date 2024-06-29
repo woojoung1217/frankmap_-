6ec337be205f3077cc1907eb0772175985713846
Dain <danggin.min@gmail.com>
2024-06-29 17:02:20 +0900

    ✨ [FE] feat: 로딩 화면 추가

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+// import "@/styles/loading.css";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="loader-wrapper">
+        <div className="loader">로딩중</div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
