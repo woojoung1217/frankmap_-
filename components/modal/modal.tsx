2258be720e81c7df5ac94b9cfd67c273b535bd6f
s0zzang <dlth8888@gmail.com>
2024-07-01 20:37:53 +0900

    💄 [FE] style: 모달 배경 스타일 지정

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -1,7 +1,7 @@
 import { useModal } from "@/hooks/useModal";
-import "@/components/modal/modal.scss"
+import "@/components/modal/modal.scss";
 import Button from "../button/button";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 
 const Modal = () => {
   const { modalDataState, closeModal } = useModal();
@@ -9,31 +9,34 @@ const Modal = () => {
 
   return (
     <>
-      { isOpen && (
-        <div className="modal-container">
-          <button className="modal-close-button" onClick={closeModal}>닫기</button>
-          { (title || emoticon) && (
-            <div className="modal-title">
-              {title && <h1>{title}</h1>}
-              {emoticon && 
-                <div>{typeof emoticon === 'string' ? parse(emoticon) : emoticon}</div>
-              }
-            </div>
-          )}
-          <div className="modal-content">
-            <div>{typeof content === 'string' ? parse(content) : content}</div>
-          </div>
-          {
-            button && (
-              <div className="modal-footer">
-                <Button handleClick={callBack} size="normal" color="secondary">{button}</Button>
+      {isOpen && (
+        <div className="modal-wrap">
+          <div className="modal-bg"></div>
+          <div className="modal-container">
+            <button className="modal-close-button" onClick={closeModal}>
+              닫기
+            </button>
+            {(title || emoticon) && (
+              <div className="modal-title">
+                {title && <h1>{title}</h1>}
+                {emoticon && <div>{typeof emoticon === "string" ? parse(emoticon) : emoticon}</div>}
               </div>
-            )
-          }
+            )}
+            <div className="modal-content">
+              <div>{typeof content === "string" ? parse(content) : content}</div>
+            </div>
+            {button && (
+              <div className="modal-footer">
+                <Button handleClick={callBack} size="normal" color="secondary">
+                  {button}
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
 export default Modal;
